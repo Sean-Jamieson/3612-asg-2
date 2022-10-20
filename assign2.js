c755eea2d224d83b29daddce78d810b25ce788a6
Sean Jamieson <71718653+Sean-Jamieson@users.noreply.github.com>
2022-10-20 10:10:35 -0600

    first push

---
+++ assign2.js
@@ -0,0 +1,13 @@
+
+
+
+/* url of song api --- https versions hopefully a little later this semester */	
+const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+
+ 
+
+/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
+   local file). To work correctly, this needs to be tested on a local web server.  
+   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+   use built-in Live Preview.
+*/
