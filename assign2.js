be480219fd16e123bed5f425f6dce967bfeafefe
Sean-Jamieson <71718653+Sean-Jamieson@users.noreply.github.com>
2023-01-17 21:38:14 -0700

    Changed to new API and adjusted some CSS

--- assign2.js
+++ assign2.js
@@ -1,10 +1,6 @@
-/* url of song api --- https versions hopefully a little later this semester 	*/
-const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
-/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.  
-   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-   use built-in Live Preview.
-*/
+
+const api = 'https://booming-bead-beryllium.glitch.me/songs';
+
 addEventListener("DOMContentLoaded", ()=>{
    if(localStorage.getItem("songList") === null){
    fetch(api)
